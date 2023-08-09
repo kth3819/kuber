5b78de6e4a4168194d412a4fe5ffeb035ef759f0
kth3819 <43310709+kth3819@users.noreply.github.com>
2023-08-09 14:43:45 +0900

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -3,7 +3,9 @@
 echo ">>>> K8S Node config Start <<<<"
 
 echo "[TASK 1] K8S Controlplane Join - API Server 192.168.10.10" 
-kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443 >/dev/null 2>&1
+cd /vagrant/
+sudo ./join.sh
+cd ~
 
 echo "[TASK 2] Config kubeconfig" 
 mkdir -p $HOME/.kube
